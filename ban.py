a4a166eff54a503f1738ef1ce7b297522c6ff521
harsha25650-debug <harsha25650@gmail.com>
2026-04-13 11:31:45 +0530

    Implement ban command for Discord bot
    Added a Ban command to the Discord bot with permissions check and embed message.

--- ban.py
+++ ban.py
@@ -1,1 +1,35 @@
+import discord
+from discord.ext import commands
 
+class Ban(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ban")
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason="No reason provided"):
+
+        if member == ctx.author:
+            return await ctx.send("You cannot ban yourself.")
+
+        if member.top_role >= ctx.author.top_role:
+            return await ctx.send("You cannot ban this user due to role hierarchy.")
+
+        try:
+            await member.ban(reason=f"{reason} | By {ctx.author}")
+
+            embed = discord.Embed(
+                title="User Banned",
+                color=discord.Color.red()
+            )
+            embed.add_field(name="User", value=str(member), inline=True)
+            embed.add_field(name="Moderator", value=ctx.author.mention, inline=True)
+            embed.add_field(name="Reason", value=reason, inline=False)
+
+            await ctx.send(embed=embed)
+
+        except discord.Forbidden:
+            await ctx.send("I don't have permission to ban this user.")
+
+async def setup(bot):
+    await bot.add_cog(Ban(bot))

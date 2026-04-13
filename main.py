2d284fcdfcd9a0c995f24ba4125263123fee3344
harsha25650-debug <harsha25650@gmail.com>
2026-04-13 11:28:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Load commands automatically
+for filename in os.listdir("./commands"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"commands.{filename[:-3]}")
+
+@bot.event
+async def on_ready():
+    print(f"Bot online as {bot.user}")
+
+bot.run(TOKEN)

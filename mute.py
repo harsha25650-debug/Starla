c52cfa29344063b52077a0ca7f6ca04ad5886df2
harsha25650-debug <harsha25650@gmail.com>
2026-04-13 11:29:59 +0530

    Update mute.py

--- mute.py
+++ mute.py
@@ -1,1 +1,23 @@
+import discord
+from discord.ext import commands
 
+class Mute(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def mute(self, ctx, member: discord.Member, *, reason=None):
+        muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+
+        if not muted_role:
+            muted_role = await ctx.guild.create_role(name="Muted")
+
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(muted_role, send_messages=False, speak=False)
+
+        await member.add_roles(muted_role)
+        await ctx.send(f"🔇 {member.mention} muted | Reason: {reason}")
+
+def setup(bot):
+    bot.add_cog(Mute(bot))

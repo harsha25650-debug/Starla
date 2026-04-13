2be5c41f8d1c68eb83f99b16cc449c2f81b21c53
harsha25650-debug <harsha25650@gmail.com>
2026-04-13 12:49:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import importlib
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,17 +9,17 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# AUTO LOAD ALL PY FILES EXCEPT MAIN
+for file in os.listdir():
+    if file.endswith(".py") and file != "main.py":
+        module_name = file[:-3]
+        module = importlib.import_module(module_name)
+
+        if hasattr(module, "setup"):
+            module.setup(bot)
+
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
-
-async def load_extensions():
-    for file in os.listdir("./commands"):
-        if file.endswith(".py"):
-            await bot.load_extension(f"commands.{file[:-3]}")
-
-@bot.event
-async def setup_hook():
-    await load_extensions()
+    print(f"Bot online as {bot.user}")
 
 bot.run(os.getenv("TOKEN"))
